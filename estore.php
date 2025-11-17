7b0cbe73cd8de74df23d59dc17ffa43651bc92ed
Garian <garianchung@gmail.com>
2025-11-17 13:48:42 +0800

    idk

--- estore.php
+++ estore.php
@@ -16,11 +16,11 @@ require_once("dbinfo.php");
 <form action="process.php" method="POST">
 
 
-    <input type="text" name="name" placeholder="Enter product name">
+    <input type="text" name="name" placeholder="Enter product name" required>
 
-    <input type="text" name="price" placeholder="Enter product price">
+    <input type="text" name="price" placeholder="Enter product price" required>
 
-    <input type="text" name="quantity" placeholder="Enter product quantity">
+    <input type="text" name="quantity" placeholder="Enter product quantity" required>
 
     <input type="submit" class="submitbtn" value="Add product">
 
