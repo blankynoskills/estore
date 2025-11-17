fac76da49ac8c9d1bb21ee40b91ed88d6979a6b6
Garian <garianchung@gmail.com>
2025-11-17 13:20:09 +0800

    Update estore.php

---
+++ estore.php
@@ -0,0 +1,15 @@
+<?php 
+$mysqli = new mysqli("localhost","root","","sep_lab7");
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Add Product</title>
+    <link rel="stylesheet" href="css/css_reset.css">
+    <link rel="stylesheet" href="css/global.css">
+    <link rel="stylesheet" href="css/editMovie.css">
+</head>
+<body>
+</body>
+</html>
