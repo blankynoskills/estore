6314029f5d25b2db2ba457d08f71860aa96f1ba2
Garian <garianchung@gmail.com>
2025-11-17 13:51:10 +0800

    Update process.php

--- process.php
+++ process.php
@@ -1,5 +1,17 @@
-<php
+<?php
 
 
+require_once("dbinfo.php");
+
+$name = $_POST['name'];
+$price = $_POST['price'];
+$quantity = $_POST['quantity'];
+
+$sql = "INSERT INTO products (Name, Price, Quantity) 
+        VALUES ('$name', '$price', '$quantity')";
+
+$mysqli->query($sql);
+
+header("location: estore.php");
 
 ?>
