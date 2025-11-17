3c0520523cf43c9d0055aa40d933b8b0c84fa0ed
Garian <garianchung@gmail.com>
2025-11-17 14:10:29 +0800

    Update estore.php

--- estore.php
+++ estore.php
@@ -20,7 +20,7 @@ require_once("dbinfo.php");
 
     <input type="text" name="price" placeholder="Enter product price" required>
 
-    <input type="number" name="quantity" placeholder="Enter product quantity" required >
+    <input type="number" min="1" name="quantity" placeholder="Enter product quantity" required >
 
     <input type="submit" class="submitbtn" value="Add product">
     
