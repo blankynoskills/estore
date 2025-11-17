e6afce74e7955efffb87f10a65d6d2b61d5bd32a
changxiuchen <changxiuchen39@gmail.com>
2025-11-17 14:00:02 +0800

    Update products.sql

--- products.sql
+++ products.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Nov 17, 2025 at 05:25 AM
+-- Generation Time: Nov 17, 2025 at 05:59 AM
 -- Server version: 9.1.0
 -- PHP Version: 8.3.14
 
@@ -34,7 +34,20 @@ CREATE TABLE IF NOT EXISTS `products` (
   `Quantity` int NOT NULL,
   `ID` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`ID`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=MyISAM AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`Name`, `Price`, `Quantity`, `ID`) VALUES
+('meow ', 12, 1, 2),
+('testing ', 1e15, 1212121212, 3),
+('312312321', 3123, 12, 4),
+('321312', 123, 13, 5),
+('product 67', 67, 67, 6),
+('test', 0, 0, 7),
+('1', 2, 3, 8);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
