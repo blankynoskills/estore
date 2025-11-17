8e4f447b33ff09553d2025df89e9900f55720758
blanky <javierlampro2@gmail.com>
2025-11-17 14:09:17 +0800

    update process.php

---
+++ process.php
@@ -0,0 +1,19 @@
+<?php
+
+
+require_once("dbinfo.php");
+
+$name = $_POST['name'];
+$price = $_POST['price'];
+$quantity = $_POST['quantity'];
+
+$sql = "INSERT INTO products (Name, Price, Quantity) 
+        VALUES ('$name', '$price', '$quantity')";
+
+$mysqli->query($sql);
+
+header("location:estore.php?&product=$name");
+
+
+
+?>
